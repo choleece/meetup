c7837bd94c684b9034febb1f6df9683180dc360a
choleece <choleece@163.com>
2018-07-26 07:54:36 +0800

    集合例子

--- src/cn/choleece/collections/CollectionDemos.java
+++ src/cn/choleece/collections/CollectionDemos.java
@@ -1,6 +1,7 @@
 package cn.choleece.collections;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 集合相关的内容 Java 集合都在java.util包下
@@ -9,4 +10,16 @@ import java.util.Collection;
  * @date 2018/7/13 12:55
  */
 public class CollectionDemos {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        Integer[] array = {3, 4, 5, 6};
+        Integer[] a = list.toArray(array);
+        System.out.println(a.length);
+        for (int aa : a) {
+            System.out.println(aa);
+        }
+    }
 }
